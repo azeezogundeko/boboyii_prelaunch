e0334c35e27c6cddf03334efdabb0714d21c328d
Engr Ogundeko <azeezogundeko19@gmail.com@example.com>
2025-10-09 23:44:14 +0200

    dockerised the app 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  async headers() {
+    return [];
+  },
+  webSocketTimeout: 30000,
+};
 
 export default nextConfig;
